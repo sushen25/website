ab67c85ed681a2849ae5cde7f2c22abfd3ff9dfb
Sushen Satturu <sushen.satturu@wspeonline.com>
2021-06-14 19:11:48 +1000

    Add BlogPost to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import BlogPost
+
 # Register your models here.
+class BlogPostAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
