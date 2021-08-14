e33403f9bfa898307c01d7f69fb379dd2a058773
Sushen Satturu <sushen.satturu@wspeonline.com>
2021-08-14 22:52:26 +1000

    Add resume page

--- sitepages/urls.py
+++ sitepages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from sitepages.views import home
+from sitepages.views import home, resume
 
 
 urlpatterns = [
     path("", home, name="home"),
+    path("resume/", resume, name="resume"),
 ]
--- sitepages/views.py
+++ sitepages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, 'home/home_page.html')
+
+
+def resume(request):
+    return render(request, 'resume/resume.html')
